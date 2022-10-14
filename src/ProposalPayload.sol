e3fb4e82413384f2b222b100ccf27fbae24cf3bc
Lukas <lukasstrassel@googlemail.com>
2022-10-14 11:44:05 +0200

    fix: add balanceOf check

--- src/ProposalPayload.sol
+++ src/ProposalPayload.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.15;
 
 import {AaveV2Ethereum} from "@aave-address-book/AaveV2Ethereum.sol";
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
 
 
 interface FeiSwapper {
@@ -18,11 +19,17 @@ interface FeiSwapper {
 contract ProposalPayload {
     address public constant FEI = 0x956F47F50A910163D8BF957Cf5846D573E7f87CA;
 
+    address public constant A_FEI = 0x683923dB55Fead99A79Fa01A27EeC3cB19679cC3;
+
     FeiSwapper public constant FEI_SWAPPER = FeiSwapper(0x9A953AC1090C7014D00FD205D89c6BA1C219Af8b);
 
     /// @notice The AAVE governance executor calls this function to implement the proposal.
     function execute() external {
         AaveV2Ethereum.POOL_CONFIGURATOR.setReserveFactor(FEI, 9_900);
-        FEI_SWAPPER.swapAllAvailable();
+        
+        // claim available liquidity and update index
+        if (IERC20(FEI).balanceOf(A_FEI) > 0) {
+            FEI_SWAPPER.swapAllAvailable();
+        }
     }
 }
